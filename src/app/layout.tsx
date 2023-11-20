232d5e2bc28e3ba747463824a9f931cfd17020fb
LH_R <song.lhlh@gmail.com>
2023-11-20 21:32:23 +0800

    feat: add @vercel/analytics and clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { AppProviders } from '@/context';
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.scss';
 
 export const metadata: Metadata = {
@@ -15,8 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="clarity-script"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "jtekf5auph");`
+        }}
+      />
       <body>
         <AppProviders>{children}</AppProviders>
+        <Analytics />
       </body>
     </html>
   );
